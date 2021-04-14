93b667c3a803b773f2808d5703dc7ba96f1973f2
sheena21 <mahaksaxenaa@gmail.com>
2021-04-14 18:02:38 +0530

    Inital setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { Navbar , NavbarBrand } from 'reactstrap';
+function App() {
+  return (
+    <div className="App">
+        <Navbar dark color="primary">
+         <div className="container">
+           <NavbarBrand href="/">XYZ</NavbarBrand>
+        </div>
+        </Navbar>
+    </div>
+  );
+}
+
+export default App;
